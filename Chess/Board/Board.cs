4afed9f156402367061fb4f9179e4fc80d773545
zpdh <36316038+zpdh@users.noreply.github.com>
2024-06-02 18:15:53 -0300

    Start to implement board

---
+++ Chess/Board/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Board
+{
+    internal class Board
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Piece[,] Pieces;
+
+        public Board(int row, int colum)
+        {
+            Row = row;
+            Column = colum;
+            Pieces = new Piece[row, colum];
+        }
+    }
+}
---
+++ Chess/Board/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Board
+{
+    internal class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int MoveQtt { get; set; }
+        public Board Board { get; set; }
+
+        public Piece(Position position, Board board, Color color)
+        {
+            Position=position;
+            Color=color;
+            Board=board;
+            MoveQtt = 0;
+        }
+    }
+}
