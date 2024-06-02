f4b115632af896f6ff062df80cfa588db8043d4c
zpdh <36316038+zpdh@users.noreply.github.com>
2024-06-02 18:05:59 -0300

    Create class Position

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Board
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position() { }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Row + ", " + Column;
+        }
+    }
+}
